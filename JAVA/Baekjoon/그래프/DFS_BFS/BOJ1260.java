2cd5fe410efbafaab2b7324dd67ddbec4795830b
윤석찬 <alsdalsa@gmail.com>
2025-09-15 09:04:00 +0900

    init

---
+++ JAVA/Baekjoon/그래프/DFS_BFS/BOJ1260.java
@@ -0,0 +1,15 @@
+/*
+문제 : 백준1260
+난이도 : 실버2
+유형 : 그래프, DFS, BFS
+링크 : https://www.acmicpc.net/problem/10828
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ1260 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
