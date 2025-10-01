b6d1ec088d4b381c665c90fbb81c6e9b5c2278cc
윤석찬 <alsdalsa@gmail.com>
2025-10-01 16:07:33 +0900

    test

--- JAVA/Programmers/PG82612.java
+++ JAVA/Programmers/PG82612.java
@@ -2,6 +2,15 @@
 public class PG82612 {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("test");
 	}
+}
+
+
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+
+        return answer;
+    }
 }
