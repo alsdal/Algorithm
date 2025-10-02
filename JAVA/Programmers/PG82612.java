e90cedcc4f3cf2abe685de83047ccf8a628c375b
alsdal <alsdalsa@gmail.com>
2025-10-02 12:26:11 +0900

    부족한 금액 채우기

--- JAVA/Programmers/PG82612.java
+++ JAVA/Programmers/PG82612.java
@@ -1,16 +1,26 @@
+/*
+문제 : 부족한 금액 계산하기
+난이도 : 1
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/82612
+ */
+
 
 public class PG82612 {
-
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println(solution(3,20,4));
 	}
-}
-
-
-class Solution {
-    public long solution(int price, int money, int count) {
-        long answer = -1;
-
+	
+    public static long solution(int price, int money, int count) {
+        long answer, temp = 0, total = 0;     
+        for (int i = 1; i <= count; i++) {
+			temp += i;
+		}
+        total = (temp * price);
+        if(total < money) {
+        		answer = 0;
+        } else {
+        		answer = total - money;
+        }
         return answer;
     }
 }
