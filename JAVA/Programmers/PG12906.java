712f2a4bc0fab9b78d90e60a28444410e868fd10
윤석찬 <alsdalsa@gmail.com>
2025-10-02 17:39:35 +0900

    같은 숫자는 싫어

--- JAVA/Programmers/PG12906.java
+++ JAVA/Programmers/PG12906.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+/*
+문제 : 같은 숫자는 싫어
+난이도 : 1
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12906
+ */
 
 public class PG12906 {
 
@@ -9,20 +16,22 @@ public class PG12906 {
 	}
 
     public static int[] solution(int []arr) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int prev = -1;
-        
-        for (int i = 0; i < arr.length; i++) {
-			if(prev != arr[i]) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(arr[0]);
+                
+        // 연속 아닌 수 추가
+        for (int i = 1; i < arr.length; i++) {
+			if(arr[i] != arr[i-1]) {
 				list.add(arr[i]);
-				prev = arr[i];
 			}
 		}
         
+        // 리스트 -> 배열 변환
         int[] answer = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
         		answer[i] = list.get(i);
         }
+        
         return answer;
     }
 }
