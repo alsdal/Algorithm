5921517a927fa8dbd8b5c5489c8656904a44ff1a
윤석찬 <alsdalsa@gmail.com>
2025-12-10 00:13:00 +0900

    구간 합 구하기 4

--- JAVA/Baekjoon/BOJ11659.java
+++ JAVA/Baekjoon/BOJ11659.java
@@ -15,24 +15,24 @@ public class BOJ11659 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		
+		// 입력 및 초기화
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-
-		int[] pfs = new int[N + 1];
+		int[] pfx = new int[N+1];
+		
 		st = new StringTokenizer(br.readLine());
-
-		for (int i = 1; i < N + 1; i++) {
-			pfs[i] = pfs[i - 1] + Integer.parseInt(st.nextToken());
+		// 미리 합 계산하여 입력 받기
+		for (int i = 1; i < N; i++) {
+			pfx[i] = pfx[i-1] + Integer.parseInt(st.nextToken());
 		}
-
-		int a = 0;
-		int b = 0;
-
+		
+	
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			sb.append(pfs[b] - pfs[a - 1]).append('\n');
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(pfx[b] - pfx[a-1]).append('\n');
 		}
 		System.out.println(sb);
 	}
