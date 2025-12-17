6f20b29c19f1ceac156568ef302c9b664f49c340
윤석찬 <alsdalsa@gmail.com>
2025-12-17 22:37:22 +0900

    최빈수 구하기

---
+++ JAVA/SWEA/SWEA1204.java
@@ -0,0 +1,15 @@
+/*
+문제 : 최빈수 구하기
+난이도 : D2
+링크 : https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV13zo1KAAACFAYh
+*/
+
+
+public class SWEA1204 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
