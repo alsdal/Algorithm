71cd49dd5796a73f8adab8f67a60c877a6b04959
윤석찬 <alsdalsa@gmail.com>
2026-01-29 19:06:09 +0900

    계산기3

--- JAVA/SWEA/SWEA1224.java
+++ JAVA/SWEA/SWEA1224.java
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-class Solution {
+class SWEA1224 {
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Stack<Character> stack = new Stack<>();
