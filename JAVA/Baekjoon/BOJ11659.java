9c3607c473b16a683f77006ce139c5083225b028
윤석찬 <alsdalsa@gmail.com>
2026-03-01 21:18:59 +0900

    구간 합 구하기 4

--- JAVA/Baekjoon/BOJ11659.java
+++ JAVA/Baekjoon/BOJ11659.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class BOJ11659 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -39,3 +38,33 @@ public class BOJ11659 {
 		System.out.println(sb);
 	}
 }
+
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11659 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+                
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N+1];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+        }
+        
+        for (int i = 0; i < M; i++) {
+        	st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(arr[b]-arr[a-1]);
+        }
+	}
+}
+*/
