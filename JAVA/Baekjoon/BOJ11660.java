cc8a81fd19f632edb7e33941c09283b4a7ab8553
윤석찬 <alsdalsa@gmail.com>
2026-03-01 22:27:32 +0900

    구간 합 구하기 5

--- JAVA/Baekjoon/BOJ11660.java
+++ JAVA/Baekjoon/BOJ11660.java
@@ -2,7 +2,7 @@
 문제 : 구간 합 구하기 5
 유형 : 누적합
 난이도 : 실버1
-링크 : https://www.acmicpc.net/problem/11659
+링크 : https://www.acmicpc.net/problem/11660
  */
 
 import java.io.BufferedReader;
@@ -14,54 +14,54 @@ public class BOJ11660 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+
+		int N = Integer.parseInt(st.nextToken()); // 표의 크기
+		int M = Integer.parseInt(st.nextToken()); // 합 구해야하는 횟수
 		int[][] arr = new int[N + 1][N + 1];
-		int[][] pfs = new int[N + 1][N + 1];
-		
+
+		// 누적합 계산
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				arr[i][j] = Integer.parseInt(st.nextToken()) + arr[i][j - 1] + arr[i - 1][j] - arr[i - 1][j - 1];
 			}
 		}
-		
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				pfs[i][j] = pfs[i][j-1] + pfs[i-1][j] - pfs[i-1][j-1] + arr[i][j];
-			}
-		}
-		
+
+		// 구간합 계산
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-		
-			int result = pfs[x2][y2] - pfs[x1-1][y2] - pfs[x2][y1-1] + pfs[x1-1][y1-1];
 
-			System.out.println(result);
+			System.out.println(arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1 - 1][y1 - 1]);
 		}
 	}
 }
 
 
 /*
+public class BOJ11660 {
 public static void main(String[] args) throws IOException {
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	StringTokenizer st = new StringTokenizer(br.readLine());
-	StringBuilder sb = new StringBuilder();
 	
 	int N = Integer.parseInt(st.nextToken());
 	int M = Integer.parseInt(st.nextToken());
+	int[][] arr = new int[N + 1][N + 1];
 	int[][] pfs = new int[N + 1][N + 1];
 	
 	for (int i = 1; i <= N; i++) {
 		st = new StringTokenizer(br.readLine());
 		for (int j = 1; j <= N; j++) {
-			pfs[i][j] = pfs[i][j - 1] + Integer.parseInt(st.nextToken());
+			arr[i][j] = Integer.parseInt(st.nextToken());
+		}
+	}
+	
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			pfs[i][j] = pfs[i][j-1] + pfs[i-1][j] - pfs[i-1][j-1] + arr[i][j];
 		}
 	}
 	
@@ -72,12 +72,10 @@ public static void main(String[] args) throws IOException {
 		int x2 = Integer.parseInt(st.nextToken());
 		int y2 = Integer.parseInt(st.nextToken());
 	
-		int sum = 0;
-		for (int j = x1; j <= x2; j++) {
-			sum += pfs[j][y2] - pfs[j][y1 - 1];
-		}
-		sb.append(sum).append('\n');
+		int result = pfs[x2][y2] - pfs[x1-1][y2] - pfs[x2][y1-1] + pfs[x1-1][y1-1];
+
+		System.out.println(result);
 	}
-	System.out.println(sb);
+}
 }
 */
