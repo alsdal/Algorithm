bd8a56770b47d4fd09df3ea2d97b90f75cf5a1e0
윤석찬 <alsdalsa@gmail.com>
2026-03-04 18:44:45 +0900

    주몽

--- JAVA/Baekjoon/BOJ1940.java
+++ JAVA/Baekjoon/BOJ1940.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class BOJ1940 {
@@ -16,34 +17,36 @@ public class BOJ1940 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-
+		int N = Integer.parseInt(br.readLine()); // 재료의 개수
+		int M = Integer.parseInt(br.readLine()); // 갑옷이 만들어지는 번호의 합
+		int[] arr = new int[N]; // 재료 번호 담을 배열
+		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-
+		
 		Arrays.sort(arr);
 		
-		int start = 0;
-		int end = N - 1;
+		int s = 0;
+		int e = N-1;
 		int cnt = 0;
-
-		while (start < end) {
-			int sum = arr[start] + arr[end];
-			if(sum == M) {
-				end--;
+		
+		while(s < e) {
+			int sum = arr[s] + arr[e];
+			if (sum == M) {
 				cnt++;
+				s++;
+				e--;
 			}
-			else if(sum < M) {
-				start++;
+			else if (sum < M) {
+				s++;
 			}
-			else {
-				end--;
+			else if(sum > M) {
+				e--;
 			}
 		}
+		
 		System.out.println(cnt);
 	}
 }
