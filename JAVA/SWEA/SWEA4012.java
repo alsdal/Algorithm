21f678c571be4d301f95d39a071714b8b04699d7
윤석찬 <alsdalsa@gmail.com>
2026-03-11 11:57:10 +0900

    요리사

--- JAVA/SWEA/SWEA4012.java
+++ JAVA/SWEA/SWEA4012.java
@@ -8,31 +8,70 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class SWEA4012
-{
-	public static int[][] graph;
-	public static void main(String args[]) throws Exception
-	{
-		System.setIn(new FileInputStream("src/input4.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int test_case = 1; test_case <= 1; test_case++)
-		{	
-			int N = Integer.parseInt(br.readLine());
-			graph = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					graph[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			
-			
-			
-		}
-	}
+public class SWEA4012 {
+
+    static int N;
+    static int[][] S;
+    static boolean[] selected;
+    static int answer;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc = 1; tc <= T; tc++) {
+
+            N = Integer.parseInt(br.readLine());
+            S = new int[N][N];
+            selected = new boolean[N];
+            answer = Integer.MAX_VALUE;
+
+            for(int i=0;i<N;i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for(int j=0;j<N;j++){
+                    S[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            comb(0,0);
+
+            System.out.println("#" + tc + " " + answer);
+        }
+    }
+
+    static void comb(int idx, int cnt){
+
+        if(cnt == N/2){
+            calc();
+            return;
+        }
+
+        for(int i=idx;i<N;i++){
+            selected[i] = true;
+            comb(i+1, cnt+1);
+            selected[i] = false;
+        }
+    }
+
+    static void calc(){
+
+        int A = 0;
+        int B = 0;
+
+        for(int i=0;i<N;i++){
+            for(int j=i+1;j<N;j++){
+
+                if(selected[i] && selected[j]){
+                    A += S[i][j] + S[j][i];
+                }
+
+                else if(!selected[i] && !selected[j]){
+                    B += S[i][j] + S[j][i];
+                }
+            }
+        }
+
+        answer = Math.min(answer, Math.abs(A-B));
+    }
 }
