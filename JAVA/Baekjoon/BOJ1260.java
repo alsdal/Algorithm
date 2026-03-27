c0db3c5c35217e8c0423ea3e142bbcacbd498369
윤석찬 <alsdalsa@gmail.com>
2026-03-27 21:58:18 +0900

    DFS와 BFS

--- JAVA/Baekjoon/BOJ1260.java
+++ JAVA/Baekjoon/BOJ1260.java
@@ -8,69 +8,79 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ1260 {
 	static boolean visited[];
-	static int[][] graph;
-	static StringBuilder sb;
-	
+	static List<Integer>[] graph;
+	static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int V = Integer.parseInt(st.nextToken());
-		graph = new int[N+1][N+1];
-		
+
+		int N = Integer.parseInt(st.nextToken()); // 정점의 개수
+		int M = Integer.parseInt(st.nextToken()); // 간선의 개수
+		int V = Integer.parseInt(st.nextToken()); // 탐색 시작 정점 번호
+
+		graph = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
+		}
+
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			graph[s][e] = graph[e][s] = 1;	
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			graph[u].add(v);
+			graph[v].add(u);
 		}
 		
-		visited = new boolean[N+1];
-		sb = new StringBuilder();
-		dfs(V);
-		System.out.println(sb);
+		// 정렬하여 작은 수부터 출력
+		for (int i = 1; i <= N; i++) {
+		    Collections.sort(graph[i]);
+		}
 
-		
-		visited = new boolean[N+1];
-		sb = new StringBuilder();
+		visited = new boolean[N + 1];
+		dfs(V);
+		sb.append("\n");
+		visited = new boolean[N + 1];
 		bfs(V);
+
 		System.out.println(sb);
-		
 	}
-	
+
 	public static void dfs(int v) {
 		visited[v] = true;
-		sb.append(v).append(" ");
-		for (int i = 1; i < graph.length; i++) {
-			if (graph[v][i] == 1 & !visited[i]) {
+		sb.append(v + " ");
+		for (int i : graph[v]) {
+			if (!visited[i]) {
 				dfs(i);
 			}
 		}
 	}
-	
+
 	public static void bfs(int v) {
 		Queue<Integer> queue = new LinkedList<>();
-		visited[v] = true;
 		queue.add(v);
-		sb.append(v).append(" ");
-		
+		visited[v] = true;
+		sb.append(v + " ");
+
 		while (!queue.isEmpty()) {
-			int pop = queue.poll();
-			for (int i = 1; i < graph.length; i++) {
-				if (graph[pop][i] == 1 && !visited[i]) {
+			int next = queue.poll();
+			for (int i : graph[next]) {
+				if (!visited[i]) {
 					queue.add(i);
 					visited[i] = true;
-					sb.append(i).append(" ");
+					sb.append(i + " ");
 				}
 			}
+
 		}
 	}
 }
