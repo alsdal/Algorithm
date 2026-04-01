94e9bffba524cbd1f17304cb5b2e5ed81924159c
윤석찬 <alsdalsa@gmail.com>
2026-04-01 23:05:00 +0900

    기타 레슨

--- JAVA/Baekjoon/BOJ2343.java
+++ JAVA/Baekjoon/BOJ2343.java
@@ -17,27 +17,24 @@ public class BOJ2343 {
 
 		int N = Integer.parseInt(st.nextToken()); // 강의의 수
 		int M = Integer.parseInt(st.nextToken()); // 블루레이의 수
-		st = new StringTokenizer(br.readLine());
-
-		int lectures[] = new int[N]; // 강의
+		int[] lectures = new int[N];
 		int start = 0;
 		int end = 0;
-		int mid = 0;
 
-		// 강의 배열 입력 및 이분 탐색 시작과 끝 설정
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			lectures[i] = Integer.parseInt(st.nextToken());
-			start = Math.max(start, lectures[i]);
-			end += lectures[i];
+			start = Math.max(start, lectures[i]); // 가능한 블루레이 최소 크기
+			end += lectures[i]; // 가능한 블루레이 최대 크기
 		}
 
-		// 블루레이 크기 mid 최소값 찾기 위해 이분 탐색
+		// 블루레이의 최소 크기 찾기 위해 이분 탐색
 		while (start <= end) {
+			int cnt = 1; // 블루레이 개수
 			int sum = 0; // 강의 크기 부분합
-			int cnt = 1; // 필요한 블루레이 개수
-			mid = (start + end) / 2;
+			int mid = (start + end) / 2;
 
-			// 블루레이에 강의 담아서 필요한 블루레이 개수 세기
+			// 블루레이 크기가 mid일 때 강의 담아서 필요한 블루레이 개수 세기
 			for (int i = 0; i < N; i++) {
 				if (sum + lectures[i] > mid) {
 					cnt++;
@@ -45,11 +42,11 @@ public class BOJ2343 {
 				}
 				sum += lectures[i];
 			}
-
-			// 이분 탐색
+			
+			// 이분탐색
 			if (cnt <= M) {
 				end = mid - 1;
-			} else if (cnt > M) {
+			} else {
 				start = mid + 1;
 			}
 		}
