c5b405406f8366fa7c83f5a8f14465fee4cf2829
윤석찬 <alsdalsa@gmail.com>
2026-04-02 16:00:56 +0900

    특정 거리의 도시 찾기

--- JAVA/Baekjoon/BOJ18352.java
+++ JAVA/Baekjoon/BOJ18352.java
@@ -9,33 +9,67 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ18352 {
 	static int N, M, K, X;
 	static List<Integer>[] list;
-
+	static int[] visited;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		N = Integer.parseInt(st.nextToken()); // 도시의 개수
 		M = Integer.parseInt(st.nextToken()); // 도로의 개수
 		K = Integer.parseInt(st.nextToken()); // 거리 정보
 		X = Integer.parseInt(st.nextToken()); // 출발 도시 번호
+		
 		list = new ArrayList[N + 1];
-
+		visited = new int[N + 1];
+		
 		for (int i = 1; i <= N; i++) {
 			list[i] = new ArrayList<>();
 		}
 
+		// 도로 입력
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int A = Integer.parseInt(st.nextToken()); // 출발 도시
 			int B = Integer.parseInt(st.nextToken()); // 도착 도시
-
+			list[A].add(B);
 		}
-
+		
+		bfs(X);
+		
+		// 정답 출력
+		boolean pathExists = false;
+		for (int i = 1; i <= N; i++) {
+			if(visited[i] == K) {
+				pathExists = true;
+				System.out.println(i);
+			}
+		}
+		if(!pathExists) {
+			System.out.println(-1);
+		}
+	}
+	
+	public static void bfs(int X) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(X);
+		
+		while(!queue.isEmpty()) {
+			int cur = queue.poll();
+			for (int i : list[cur]) {
+				if(visited[i] == 0) {
+					queue.add(i);
+					visited[i] = visited[cur] + 1;
+				}
+			}
+		}		
 	}
 }
