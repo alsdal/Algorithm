39718e10e148915fcda216c0984f9e407e1513f0
윤석찬 <alsdalsa@gmail.com>
2026-04-05 17:31:32 +0200

    숫자 카드

--- JAVA/Baekjoon/BOJ10815.java
+++ JAVA/Baekjoon/BOJ10815.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ10815 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException { 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
