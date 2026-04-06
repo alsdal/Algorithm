b3a86817e4a9b325ef1735d3ca4cf51f78fead96
윤석찬 <alsdalsa@gmail.com>
2026-04-06 18:17:07 +0200

    숫자 카드

--- JAVA/Baekjoon/BOJ10815.java
+++ JAVA/Baekjoon/BOJ10815.java
@@ -12,43 +12,52 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ10815 {
-	public static void main(String[] args) throws IOException { 
+	static int[] cards;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-
+		StringBuilder sb = new StringBuilder();
+		
 		int N = Integer.parseInt(br.readLine()); // 숫자 카드 개수
-		int[] cards = new int[N];
+		st = new StringTokenizer(br.readLine()); // 숫자 카드에 적힌 수
+		cards = new int[N];
 
-		st = new StringTokenizer(br.readLine()); // 숫자 카드에 적힌 정수
 		for (int i = 0; i < N; i++) {
 			cards[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(cards);
 
-		int M = Integer.parseInt(br.readLine()); // 구해야할 정수 개수
-		st = new StringTokenizer(br.readLine()); // 구해야하는 정수
-		StringBuilder sb = new StringBuilder();
+		int M = Integer.parseInt(br.readLine()); // 구할 숫자 카드 개수
+		st = new StringTokenizer(br.readLine()); // 구할 숫자 카드
+
 		for (int i = 0; i < M; i++) {
 			int num = Integer.parseInt(st.nextToken());
-			sb.append(hasCard(cards, num)).append(" ");
+			int s = 0;
+			int e = cards.length - 1;
+
+			boolean hasCard = false;
+			while (s <= e) {
+				int m = (s + e) / 2;
+
+				if (cards[m] < num) {
+					s = m + 1;
+				} else if (cards[m] > num) {
+					e = m - 1;
+				} else {
+					hasCard = true;
+					break;
+				}
+			}
+			
+			if(hasCard) {
+				sb.append(1 + " ");
+			}
+			else {
+				sb.append(0 + " ");
+			}
+
 		}
 		System.out.println(sb);
-	}
-
-	public static int hasCard(int[] cards, int key) {
-		int s = 0;
-		int e = cards.length - 1;
-
-		while (s <= e) {
-			int m = (s + e) / 2;
-			if (key < cards[m]) {
-				e = m - 1;
-			} else if (key > cards[m]) {
-				s = m + 1;
-			} else {
-				return 1;
-			}
-		}
-		return 0;
 	}
 }
