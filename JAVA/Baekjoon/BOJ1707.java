e05998e9ad63c93b35dc146db0bb47505fa77fa7
윤석찬 <alsdalsa@gmail.com>
2026-04-14 23:59:22 +0900

    이분 그래프

--- JAVA/Baekjoon/BOJ1707.java
+++ JAVA/Baekjoon/BOJ1707.java
@@ -9,12 +9,70 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Stack;
+import java.util.Iterator;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ1707 {
+	static List<Integer>[] list;
+	static int[] visited;
+	static boolean ans;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringTokenizer st;
+
+		int K = Integer.parseInt(br.readLine()); // 테스트 케이스 수
+
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			int V = Integer.parseInt(st.nextToken()); // 정점의 개수
+			int E = Integer.parseInt(st.nextToken()); // 간선의 개수
+
+			list = new ArrayList[V + 1];
+			for (int j = 1; j <= V; j++) {
+				list[j] = new ArrayList<Integer>();
+			}
+			visited = new int[V + 1];
+
+			for (int j = 0; j < E; j++) {
+				st = new StringTokenizer(br.readLine());
+				int u = Integer.parseInt(st.nextToken());
+				int v = Integer.parseInt(st.nextToken());
+				list[u].add(v);
+				list[v].add(u);
+			}
+
+			ans = true;
+			for (int j = 1; j <= V; j++) {
+				if (visited[j] == 0) {
+					dfs(j);
+					if(!ans) {
+						break;
+					}
+				}
+			}
+
+			if (ans) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+		}
+	}
+
+	public static void dfs(int v) {
+		visited[v] = 1;
+		for (int i : list[v]) {
+			if(visited[i] == 0) {
+				visited[i] = visited[v] * (-1);
+				dfs(i);
+			}
+			else if (visited[i] == visited[v]) {
+				ans = false;
+				break;
+			}
+		}
+
 	}
 }
