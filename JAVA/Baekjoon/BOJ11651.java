4317c9dd3a7445faedf62de9211002dc1cce2add
윤석찬 <alsdalsa@gmail.com>
2026-04-18 18:12:26 +0900

    좌표 정렬하기 2

--- JAVA/Baekjoon/BOJ11651.java
+++ JAVA/Baekjoon/BOJ11651.java
@@ -36,17 +36,9 @@ public class BOJ11651 {
 			}
 		});
 
-//		Arrays.sort(arr, (e1, e2) -> {
-//			if(e1[0] == e2[0]) {
-//				return e1[1] - e2[1];
-//			} else {
-//				return e1[0] - e2[0];
-//			}
-//		});
-		
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < N; i++) {
-			sb.append(arr[i][0]+" "+arr[i][1]+'\n');
+			sb.append(arr[i][0] + " " + arr[i][1] + '\n');
 		}
 		System.out.println(sb);
 	}
