f7162dd4246f002623ecea6a32e42843a8d0641b
윤석찬 <alsdalsa@gmail.com>
2026-04-27 23:50:35 +0900

    소수 구하기

--- JAVA/Baekjoon/BOJ1929.java
+++ JAVA/Baekjoon/BOJ1929.java
@@ -8,6 +8,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class BOJ1929 {
@@ -15,28 +16,29 @@ public class BOJ1929 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		int arr[] = new int[N + 1];
-
+		int M = Integer.parseInt(st.nextToken()); // M 이상
+		int N = Integer.parseInt(st.nextToken()); // N 이하
+		int[] arr = new int[N+1];
+		
+		// 배열 초기화
 		for (int i = 2; i <= N; i++) {
 			arr[i] = i;
 		}
-
-		for (int i = 2; i <= Math.sqrt(N); i++) {
-			if (arr[i] == 0) {
+		
+		for (int i = 2; i < Math.sqrt(N); i++) {
+			if(arr[i] == 0) {
 				continue;
 			}
 			for (int j = 2 * i; j <= N; j = j + i) {
 				arr[j] = 0;
 			}
 		}
-
+		
 		for (int i = M; i <= N; i++) {
 			if (arr[i] != 0) {
 				System.out.println(arr[i]);
 			}
 		}
-
+	
 	}
 }
