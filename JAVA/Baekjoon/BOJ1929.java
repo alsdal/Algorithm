e51c8cb9d85a4df23d4213b0038648a26e4fc2da
윤석찬 <alsdalsa@gmail.com>
2026-04-28 23:56:36 +0900

    소수 구하기

--- JAVA/Baekjoon/BOJ1929.java
+++ JAVA/Baekjoon/BOJ1929.java
@@ -8,39 +8,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ1929 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
 
-		int M = Integer.parseInt(st.nextToken()); // M 이상
-		int N = Integer.parseInt(st.nextToken()); // N 이하
-		int[] arr = new int[N+1];
-		
-		// 배열 초기화
-		for (int i = 2; i <= N; i++) {
-			arr[i] = i;
-		}
-		
-		for (int i = 2; i < Math.sqrt(N); i++) {
-			if(arr[i] == 0) {
-				continue;
-			}
-			// 배수 지우기
-			for (int j = 2 * i; j <= N; j = j + i) {
-				arr[j] = 0;
+		// 소수이면 true인 boolean 배열
+		boolean[] isPrime = new boolean[N + 1];
+		Arrays.fill(isPrime, true);
+		isPrime[0] = isPrime[1] = false;
+
+		// 제곱근까지 수행
+		for (int i = 2; i * i <= N; i++) {
+			// i의 배수 == 소수 아닌 값 false 처리
+			if (isPrime[i]) {
+				for (int j = i * i; j <= N; j += i) {
+					isPrime[j] = false;
+				}
 			}
 		}
-		
-		// 소수 출력
+
+		// 소수값 출력
 		for (int i = M; i <= N; i++) {
-			if (arr[i] != 0) {
-				System.out.println(arr[i]);
+			if (isPrime[i]) {
+				System.out.println(i);
 			}
 		}
-	
 	}
 }
