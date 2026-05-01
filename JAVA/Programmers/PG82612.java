e8dac76918d91f295c31c2a6a808fd32fcd5ebb0
윤석찬 <alsdalsa@gmail.com>
2026-05-01 18:16:24 +0900

    부족한 금액 계산하기

--- JAVA/Programmers/PG82612.java
+++ JAVA/Programmers/PG82612.java
@@ -4,23 +4,27 @@
 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/82612
  */
 
-
 public class PG82612 {
 	public static void main(String[] args) {
-		System.out.println(solution(3,20,4));
+		System.out.println(solution(3, 20, 4));
 	}
-	
-    public static long solution(int price, int money, int count) {
-        long answer, temp = 0, total = 0;     
-        for (int i = 1; i <= count; i++) {
+
+	public static long solution(int price, int money, int count) {
+		long answer = 0;
+		long temp = 0;
+		long total = 0;
+
+		for (int i = 1; i <= count; i++) {
 			temp += i;
 		}
-        total = (temp * price);
-        if(total < money) {
-        		answer = 0;
-        } else {
-        		answer = total - money;
-        }
-        return answer;
-    }
+
+		total = (temp * price);
+
+		if (total < money) {
+			answer = 0;
+		} else {
+			answer = total - money;
+		}
+		return answer;
+	}
 }
