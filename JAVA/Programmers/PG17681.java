09b25de2b179dc9f927cfb203f5c1f6fc64f095d
윤석찬 <alsdalsa@gmail.com>
2026-05-16 21:49:02 +0900

    비밀지도

--- JAVA/Programmers/PG17681.java
+++ JAVA/Programmers/PG17681.java
@@ -8,6 +8,7 @@ public class PG17681 {
 	public static void main(String[] args) {
 //		String[] string = solution(5, new int[] { 9, 20, 28, 18, 11 }, new int[] { 30, 1, 21, 17, 28 });
 		String[] string = solution(6, new int[] { 46, 33, 33, 22, 31, 50 }, new int[] { 27, 56, 19, 14, 14, 10 });
+
 		for (int i = 0; i < string.length; i++) {
 			System.out.println(string[i]);
 		}
