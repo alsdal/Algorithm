a1622886f7528650a685f91a7e0b7da09c28b133
윤석찬 <alsdalsa@gmail.com>
2026-05-30 20:14:30 +0900

    기사단원의 무기

--- JAVA/Programmers/PG136798.java
+++ JAVA/Programmers/PG136798.java
@@ -16,9 +16,13 @@ public class PG136798 {
 		for (int i = 1; i <= number; i++) {
 			int cnt = 0;
 
-			for (int j = 1; j <= i; j++) {
+			for (int j = 1; j * j <= i; j++) {
 				if (i % j == 0) {
-					cnt++;
+					if (j * j == i) {
+						cnt++;
+					} else {
+						cnt += 2;
+					}
 				}
 			}
 
