52dd03c3a24c5e77b1e32e82795b734808aaae67
윤석찬 <alsdalsa@gmail.com>
2026-06-06 19:03:11 +0900

    삼총사

---
+++ JAVA/Programmers/PG131705.java
@@ -0,0 +1,26 @@
+/*
+문제 : 문자열을 정수로 바꾸기
+난이도 : 1
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/131705
+*/
+
+public class PG131705 {
+	public static void main(String[] args) {
+		System.out.println(solution(new int[] { -3, -2, -1, 0, 1, 2, 3 }));
+	}
+
+	public static int solution(int[] number) {
+		int answer = 0;
+		for (int i = 0; i < number.length - 2; i++) {
+			for (int j = i + 1; j < number.length - 1; j++) {
+				for (int k = j + 1; k < number.length; k++) {
+					if (number[i] + number[j] + number[k] == 0) {
+						answer++;
+					}
+				}
+			}
+		}
+
+		return answer;
+	}
+}
