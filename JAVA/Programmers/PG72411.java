bcbd2b760150aa098a460f35e59358f68f895de7
윤석찬 <alsdalsa@gmail.com>
2026-06-28 19:34:08 +0900

    메뉴 리뉴얼

---
+++ JAVA/Programmers/PG72411.java
@@ -0,0 +1,25 @@
+/*
+문제 : 메뉴 리뉴얼
+난이도 : 2
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/72411
+*/
+
+public class PG72411 {
+	public static void main(String[] args) {
+		String[] arr = solution(new String[] { "ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH" }, new int[] { 2, 3, 4 });
+	}
+
+	public static String[] solution(String[] orders, int[] course) {
+		String[] answer = {};
+		
+		for (String menu : orders) {
+			for (int i = 2; i < menu.length(); i++) {
+				
+			}
+		}
+		
+		
+		
+		return answer;
+	}
+}
