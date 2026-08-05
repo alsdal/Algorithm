67e0edde1000707a4c5332e83783beab3ad1496f
윤석찬 <alsdalsa@gmail.com>
2026-08-05 16:31:03 +0900

    요격 시스템

---
+++ JAVA/Programmers/PG181188.java
@@ -0,0 +1,39 @@
+/*
+문제 : 요격 시스템
+난이도 : 2
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181188
+*/
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class PG181188 {
+	public static void main(String[] args) {
+		System.out.println(
+				solution(new int[][] { { 4, 5 }, { 4, 8 }, { 10, 14 }, { 11, 13 }, { 5, 12 }, { 3, 7 }, { 1, 4 } }));
+	}
+
+	public static int solution(int[][] targets) {
+		int answer = 0;
+
+		Arrays.sort(targets, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] a, int[] b) {
+				return a[1] - b[1];
+			}
+		});
+
+		int last = -1;
+
+		for (int[] target : targets) {
+			int start = target[0];
+			int end = target[1];
+			if (start >= last) {
+				last = end;
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+}
