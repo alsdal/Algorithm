ba9c6488710081159f314830cd80c45e14845705
윤석찬 <alsdalsa@gmail.com>
2026-08-08 22:20:20 +0900

    이진 변환 반복하기

---
+++ JAVA/Programmers/PG70129.java
@@ -0,0 +1,34 @@
+/*
+문제 : 이진 변환 반복하기
+난이도 : 2
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/70129
+*/
+
+public class PG70129 {
+	public static void main(String[] args) {
+		int[] arr = solution("110010101001");
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+
+	public static int[] solution(String s) {
+		int[] answer = {};
+		int zeroCnt = 0;
+		int convertCnt = 0;
+		while (!s.equals("1")) {
+			int oneCnt = 0;
+			for (int i = 0; i < s.length(); i++) {
+				if (s.charAt(i) == '1') {
+					oneCnt++;
+				} else {
+					zeroCnt++;
+				}
+			}
+
+			s = Integer.toBinaryString(oneCnt);
+			convertCnt++;
+		}
+		return new int[] { convertCnt, zeroCnt };
+	}
+}
