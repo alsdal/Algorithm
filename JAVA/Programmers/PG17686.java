188a44f074473af5b821aa41577f2a66b0a5cc18
윤석찬 <alsdalsa@gmail.com>
2026-08-12 18:17:53 +0900

    파일명 정렬

---
+++ JAVA/Programmers/PG17686.java
@@ -0,0 +1,78 @@
+/*
+문제 : 파일명 정렬
+난이도 : 2
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/17686
+*/
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class PG17686 {
+	public static void main(String[] args) {
+		String[] arr = solution(
+				new String[] { "img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG" });
+
+		for (String s : arr) {
+			System.out.println(s);
+		}
+	}
+
+	static class File {
+		String name;
+		String head;
+		int number;
+
+		File(String name, String head, int number) {
+			this.name = name;
+			this.head = head;
+			this.number = number;
+		}
+	}
+
+	public static String[] solution(String[] files) {
+		File[] arr = new File[files.length];
+
+		// 파일명 분리
+		for (int i = 0; i < files.length; i++) {
+			String file = files[i];
+
+			// number 시작 지점 찾기
+			int numberStart = 0;
+			while (!Character.isDigit(file.charAt(numberStart))) {
+				numberStart++;
+			}
+
+			// number 끝 지점 찾기
+			int numberEnd = numberStart;
+			while (numberEnd < file.length() && Character.isDigit(file.charAt(numberEnd))) {
+				numberEnd++;
+			}
+
+			String head = file.substring(0, numberStart);
+			int number = Integer.parseInt(file.substring(numberStart, numberEnd));
+
+			arr[i] = new File(file, head, number);
+		}
+
+		Arrays.sort(arr, new Comparator<File>() {
+			@Override
+			public int compare(File a, File b) {
+				// head 비교
+				int headCompare = a.head.toLowerCase().compareTo(b.head.toLowerCase());
+				if (headCompare != 0) {
+					return headCompare;
+				}
+
+				return a.number - b.number;
+			}
+		});
+
+		String[] answer = new String[files.length];
+
+		for (int i = 0; i < arr.length; i++) {
+			answer[i] = arr[i].name;
+		}
+
+		return answer;
+	}
+}
