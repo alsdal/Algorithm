c6fc0c99b9c32af4f5c2abd01f53808fcc3cc10d
윤석찬 <alsdalsa@gmail.com>
2026-08-15 17:38:24 +0900

    햄버거 만들기

--- JAVA/Programmers/PG133502.java
+++ JAVA/Programmers/PG133502.java
@@ -21,9 +21,11 @@ public class PG133502 {
 
 			int size = stack.size();
 
+			// 햄버거를 만들 수 있는 경우
 			if (stack.size() >= 4 && stack.get(stack.size() - 4) == 1 && stack.get(stack.size() - 3) == 2
 					&& stack.get(stack.size() - 2) == 3 && stack.get(stack.size() - 1) == 1) {
-
+				
+				// 스택에서 제거
 				for (int i = 0; i < 4; i++) {
 					stack.pop();
 				}
