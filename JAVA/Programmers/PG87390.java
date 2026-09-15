26c0cf83f35ce8f0f9ae79f22a2fea4dbf90a622
윤석찬 <alsdalsa@gmail.com>
2026-09-16 00:17:46 +0900

    n^2 배열 자르기

--- JAVA/Programmers/PG87390.java
+++ JAVA/Programmers/PG87390.java
@@ -6,6 +6,24 @@
 
 public class PG87390 {
 	public static void main(String[] args) {
+		int[] arr = solution(2, 2, 5);
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
 
+	public static int[] solution(int n, long left, long right) {
+		int size = (int) (right - left + 1);
+		int[] answer = new int[size];
+
+		for (int i = 0; i < size; i++) {
+			long idx = left + i;
+
+			long row = idx / n;
+			long col = idx % n;
+
+			answer[i] = (int) (Math.max(row, col) + 1);
+		}
+		return answer;
 	}
 }
