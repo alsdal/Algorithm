d8bee0b31ec21303051605bca5a84c75b0f723b7
윤석찬 <alsdalsa@gmail.com>
2026-09-16 22:32:25 +0900

    무인도 여행

---
+++ JAVA/Programmers/PG154540.java
@@ -0,0 +1,19 @@
+/*
+문제 : 무인도 여행
+난이도 : 2
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/154540
+*/
+
+public class PG154540 {
+	public static void main(String[] args) {
+		int[] arr = solution(new String[] { "X591X", "X1X5X", "X231X", "1XXX1" });
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+
+	public static int[] solution(String[] maps) {
+		int[] answer = {};
+		return answer;
+	}
+}
