da3c99895cd564b0b072929127cc521235333468
윤석찬 <alsdalsa@gmail.com>
2026-09-19 19:37:56 +0900

    튜플

---
+++ JAVA/Programmers/PG64065.java
@@ -0,0 +1,52 @@
+/*
+문제 : 튜플
+난이도 : 2
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/64065
+*/
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PG64065 {
+	public static void main(String[] args) {
+		int[] arr = solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+
+	public static int[] solution(String s) {
+		s = s.substring(2, s.length() - 2);
+		String[] groups = s.split("\\},\\{");
+
+		String[][] numbers = new String[groups.length][];
+
+		for (int i = 0; i < groups.length; i++) {
+			numbers[i] = groups[i].split(",");
+		}
+
+		Arrays.sort(numbers, new Comparator<String[]>() {
+			@Override
+			public int compare(String[] a, String[] b) {
+				return Integer.compare(a.length, b.length);
+			}
+		});
+
+		int[] answer = new int[groups.length];
+		int idx = 0;
+		Set<Integer> set = new HashSet<>();
+
+		for (String[] group : numbers) {
+			for (String str : group) {
+				int num = Integer.parseInt(str);
+				if (set.add(num)) {
+					answer[idx++] = num;
+				}
+			}
+		}
+
+		return answer;
+	}
+}
