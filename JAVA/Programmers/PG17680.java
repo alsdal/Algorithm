4588eec8b2e104f38e97faca434e44e6555aa418
윤석찬 <alsdalsa@gmail.com>
2026-09-22 23:57:19 +0900

    캐시

---
+++ JAVA/Programmers/PG17680.java
@@ -0,0 +1,17 @@
+/*
+문제 : 캐시
+난이도 : 2
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/17680
+*/
+
+public class PG17680 {
+	public static void main(String[] args) {
+		System.out.println(solution(3, new String[] { "Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo",
+				"Seoul", "NewYork", "LA" }));
+	}
+
+	public static int solution(int cacheSize, String[] cities) {
+		int answer = 0;
+		return answer;
+	}
+}
