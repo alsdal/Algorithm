cd4b541c96d7541d6bba80674842b4599d93560f
윤석찬 <alsdalsa@gmail.com>
2026-09-23 00:25:45 +0900

    캐시

--- JAVA/Programmers/PG17680.java
+++ JAVA/Programmers/PG17680.java
@@ -4,6 +4,9 @@
 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/17680
 */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PG17680 {
 	public static void main(String[] args) {
 		System.out.println(solution(3, new String[] { "Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo",
@@ -12,6 +15,32 @@ public class PG17680 {
 
 	public static int solution(int cacheSize, String[] cities) {
 		int answer = 0;
+		List<String> list = new ArrayList<>();
+
+		// 캐시 크기 0인 경우 예외 처리
+		if (cacheSize == 0) {
+			return cities.length * 5;
+		}
+
+		for (String city : cities) {
+			city = city.toLowerCase();
+
+			// cache hit
+			if (list.contains(city)) {
+				answer += 1;
+				list.remove(city);
+			}
+			// cache miss
+			else {
+				answer += 5;
+				if (list.size() == cacheSize) {
+					list.remove(0);
+				}
+			}
+
+			list.add(city);
+		}
+
 		return answer;
 	}
 }
